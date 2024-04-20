e733cb4b6c3e00facaec5d510a513dc04c5cfa02
Choiseeun0815 <103297048+Choiseeun0815@users.noreply.github.com>
2024-04-20 23:23:51 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s; 
+	cin >> s;
+
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		if (s.find(i) != string::npos) {
+			cout << s.find(i) << ' ';
+		}
+		else cout << -1 << ' ';
+	}
+}
