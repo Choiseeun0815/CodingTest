dd1422179350a6d16544d23fa9d5908662819167
Choiseeun0815 <103297048+Choiseeun0815@users.noreply.github.com>
2024-05-05 20:12:49 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -2,18 +2,18 @@
 #include <algorithm>
 using namespace std;
 
+
+
 int main()
 {
 	string str, cmpStr;
 	cin >> str;
 	cmpStr = str;
 
-	for (int i = 0; i < sizeof(str) / sizeof(string); i++)
-	{
-		reverse(str.begin(), str.end());
-	}
+	reverse(str.begin(), str.end());
 
 	if (str == cmpStr)
 		cout << 1;
 	else cout << 0;
+
 }
