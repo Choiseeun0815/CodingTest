f881457b97378988452bf14c2b1454de5f16e07b
Choiseeun0815 <103297048+Choiseeun0815@users.noreply.github.com>
2024-11-18 13:29:13 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -1,29 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() 
 {
 	int N, B;
+	int remainder; // 나머지를 저장할 변수
 	string res = "";
 
 	cin >> N >> B;
 
 	while (N > 0)
 	{
-		int n = N % B;
 		char c;
+		remainder = N % B;
 
-		//N을 B로 나눈 나머지가 10 이상이라면, 
-		//이에 대해서는 알파벳 대문자로 표현해야하므로
-		//if문 안의 연산을 실행함 
-		if (n >= 10)
-			c = 'A' + (n - 10);
+		if (remainder >= 10) //문자 변화가 필요한 경우
+			c = 'A' + (remainder - 10);
 		else
-			c = '0' + n;
+			c = '0' + remainder;
 
 		res = c + res;
 		N /= B;
 	}
-
 	cout << res;
+	return 0;
 }
