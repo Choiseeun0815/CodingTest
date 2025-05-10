6f475884e6b2b2ebca8ed389a85e0280cab6e2e2
Choiseeun0815 <103297048+Choiseeun0815@users.noreply.github.com>
2025-05-10 12:22:46 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,19 +1,28 @@
 #include <string>
 #include <sstream>
-#include <limits.h>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 string solution(string s) {
-    string answer = "";
-    int max = INT_MIN, min = INT_MAX;
-    stringstream ss(s); 
-    int num;
-    
-    while (ss >> num) { 
-        if (num > max) max = num;
-        if (num < min) min = num;
-    }
-    
-    answer = to_string(min) + " " + to_string(max);
-    return answer;
+	string answer = "";
+
+	stringstream ss(s); //문자열 스트림
+	vector<string> words;
+	while (ss >> answer) //공백을 기준으로 분리된 문자를 words에 넣음
+	{
+		words.push_back(answer);
+	}
+
+	vector<int> v;
+	for (string str : words)
+	{
+		int num = stoi(str);
+		v.push_back(num);
+	}
+	int max = *max_element(v.begin(), v.end());
+	int min = *min_element(v.begin(), v.end());
+
+	answer = to_string(min) + " " + to_string(max);
+	return answer;
 }
