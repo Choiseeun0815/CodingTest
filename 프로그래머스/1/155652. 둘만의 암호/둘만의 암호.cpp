64f92666ceaa7cefb54802e2a09bddc95a7a628c
최세은 <103297048+Choiseeun0815@users.noreply.github.com>
2025-11-04 17:11:25 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -4,28 +4,20 @@ using namespace std;
 
 string solution(string s, string skip, int index) 
 {
-	string answer = "";
 	int i = 0;
-	char compare = ' ';
-	int code_a = static_cast<int>('a');
-	int code_z = static_cast<int>('z');
-
+	
 	for (int i = 0; i < s.size(); i++)
 	{
-		int num = static_cast<int>(s[i]);
 
 		for (int j = 0; j < index;)
 		{
-			num++;
-			if (num > code_z) //알파벳이 z를 넘어가면 a로 돌려놓기
-				num = code_a;
+			s[i]++;
+			if (s[i] > 'z') //알파벳이 z를 넘어가면 a로 돌려놓기
+				s[i] = 'a';
 
-			compare = static_cast<char>(num);
-
-			if (skip.find(compare) == string::npos) //스킵 문자에 존재하지 않으면,
+			if (skip.find(s[i]) == string::npos) //스킵 문자에 존재하지 않으면,
 				j++;
 		}
-		answer += compare; 
 	}
-	return answer;
+	return s;
 }
